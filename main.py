e98792cde5163bd7a3ebe739b2de21a85dfd28bf
mclacore <mclacore@gmail.com>
2024-05-20 10:28:04 -0700

    Fixing broken indexing of range

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ def session():
   session['distance'] = int(input("Add distance\n"))
   session['time'] = input("Add time taken\n")
   session['splits'] = []
-  split = "0"
-  for i in range(0, session['distance']):
-    split = split[i]
-    split = input(f"Add split for {i} mile\n")
+  split = '0'
+  
+  for i in range(1, session['distance'] + 1):
+    split = input(f"Add split for mile {i}\n")
     session['splits'].append(split)
+  
   print(f"distance is {session['distance']} and time is {session['time']}")
   print(session)
   
