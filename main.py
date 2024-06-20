5a58c9ee76806dbb70631c60cfa96bb76b81482b
mclacore <michael@massdriver.cloud>
2024-06-20 16:04:22 -0700

    grading splits, or at least trying to

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 def main():
+  print("Welcome to the running app")
+  session = session()
+  grade_splits(session)
+  print("Goodbye")
+
+
+def session():
   session = {}
   session['distance'] = int(input("Add distance\n"))
   session['time'] = input("Add time taken\n")
@@ -9,7 +16,31 @@ def main():
     split = input(f"Add split for mile {i}\n")
     session['splits'].append(split)
   
+  print(session)
   return session
+
+
+def grade_splits(session):
+  splits = session['splits']
+  counter = 0
+  for i in range(0, len(splits)):
+    split = splits[i]
+    if i <= 2:
+      if split[i + 1] <= split[1]:
+        counter += 1
+      else:
+        counter -= 1
+    elif i >= 3 and i <= 6:
+      if split[i + 1] <= split[1]:
+        counter += 2
+      else:
+        counter -= 2
+    else:
+      if split[i + 1] <= split[1]:
+        counter += 3
+      else:
+        counter -= 3
+  return counter
 
 
 def miles_to_km(distance):
